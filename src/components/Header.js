72648941abcc11190de9a81d48875752f9ef9fe0
jalarconba <148835203+jalarconba@users.noreply.github.com>
2024-05-20 01:01:21 -0400

    MODIFICANDO FORMULARIOS

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,7 +39,7 @@ const Header = () => {
         </div>
       </nav>
       <h2 className="text-center mb-3">Sistema de Registro de Asistencia Escolar</h2>
-      <Tab />
+      {/* <Tab /> */}
     </header>
   );
 };
